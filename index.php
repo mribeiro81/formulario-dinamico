a86cbbf7cfa9900b437e457af4208477c8d4fae0
mribeiro81 <mribeiro81@hotmail.com>
2020-12-06 20:22:54 -0300

    Alterei o array na index.php para se ajustar ao novo modo de criar inputs

--- index.php
+++ index.php
@@ -18,47 +18,115 @@
 
 			<?php				
 
-				require_once("vendor/autoload.php");
-
-				$dados_form= [				
-
-					"type:form|name:frmTeste|id:frmTeste|autocomplete:off|action:gravar.php|method:post|enctype:multipart/form-data|target:_self|input-class:mt-2",
-
-					"type:submit|name:enviar|id:enviar|value:Enviar|input-class:btn btn-primary btn-lg btn-block mt-4 mb-5|texto:Ao clicar no botão, o formulário será submetido, mas, a paǵina de destino não foi criada.<br>Você receberá a mensagem de página não encontrada.|tag-texto:div|class-texto:alert alert-warning mt-5|",
-
-					"type:hidden|name:token|id:token|value:",
+				require_once("vendor/autoload.php");	
 
 
-					"type:text|name:nome|id:nome|maxlength:30|label:Nome|obrigatorio:*|value:|pattern:[a-z\s]+$|title:Preencha esse campo com o seu nome|placeholder:Informe o seu nome|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
+				$dados_form= [
 
-					"type:text|name:sobrenome|id:sobrenome|maxlength:30|label:Sobrenome|obrigatorio:*|value:|pattern:[a-z\s]+$|title:Preencha esse campo com o seu sobrenome|placeholder:Informe o seu sobrenome|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
+					//Exemplo de como criar a tag form
+					"type:form|action:gravar.php|method:post|name:frmContato|id:frmContato",
 
-					"type:email|name:email|id:email|maxlength:80|label:E-mail|obrigatorio:*|value:|pattern:[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$|title:Preencha esse campo com o seu e-mail|placeholder:Informe o seu e-mail|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:password|name:senha|id:senha|maxlength:12|label:Senha para futuros acessos|obrigatorio:*|value:|pattern:|title:Informe uma senha para futuros acessos|placeholder:Informe uma senha para futuros acessos|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:tel|name:celular|id:celular|maxlength:|label:Celular|obrigatorio:*|value:|pattern:\([0-9]{2}\)[0-9]{5}-[0-9]{4}$|title:Preencha esse campo com o número do seu celular|placeholder:(00)00000-0000|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:date|name:data_nascimento|id:data_nascimento|maxlength:10|min:1940-01-01|max:2020-12-04|label:Data de nascimento|obrigatorio:*|value:|pattern:[0-9]{2}\/[0-9]{2}\/[0-9]{4}$|title:Preencha esse campo com a sua data de nascimento|placeholder:|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:select|name:sexo|id:sexo|label:Sexo|options:Selecione-Selecione F-Feminino M-Masculino|obrigatorio:*|title:Selecione uma opção|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:textarea|name:menagem|id:mensagem|maxlength:120|label:Mensagem|obrigatorio:*|value:|title:Preencha esse campo com a sua mensagem|placeholder:Escreva a sua mensagem|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
-
-					"type:radio|name:itenServico|id:servico|value:1|checked:|label:Serviço|obrigatorio:*|title:Serviço|div-class:form-check|label-class:form-check-label|input-class:form-check-input required-radio|texto:Selecione uma opção|tag-texto:h5|class-texto:mt-4|",
-
-					"type:radio|name:itenServico|id:desenvolvimento|value:2|checked:|label:Desenvolvimento|obrigatorio:*|title:Desenvolvimento|div-class:form-check|label-class:form-check-label|input-class:form-check-input required-radio|texto:|tag-texto:|class-texto:|",
-
-					"type:radio|name:itenServico|id:suporte|value:3|checked:|label:Suporte|obrigatorio:*|title:Suporte|div-class:form-check|label-class:form-check-label|input-class:form-check-input required-radio|texto:|tag-texto:|class-texto:|",
-
-					"type:checkbox|name:contatoEmail|id:contatoEmail|value:1|checked:|label:E-mail|obrigatorio:*|title:Contato por e-email|div-class:form-check|label-class:form-check-label|input-class:form-check-input required-checkbox|texto:Informe como prefere que façamos contato|tag-texto:h5|class-texto:mt-4|",
-
-					"type:checkbox|name:contatoSms|id:contatoSms|value:2|checked:|label:SMS|obrigatorio:*|title:Contato por SMS|div-class:form-check|label-class:form-check-label|input-class:form-check-input required-checkbox|texto:|tag-texto:|class-texto:|",
+					//inut hidden
+					"type:hidden|name:token|id:token|value:".md5(date("Y-m-d h:i:s"))."",
 
 
-					"type:file|name:foto|id:foto|label:Envie uma foto sua|obrigatorio:*|accept:image/*|title:Envie uma foto sua|placeholder:Envie uma foto sua|div-class:form-group mt-3|label-class:|input-class:form-control required|texto:|tag-texto:|class-texto:|",
+					"type:div|class:form-group mt-3", //abrindo div que irá envolver o input
+						"type:label|for:nome|value:Nome|obrigatorio:*", //Label do input
+						//input text
+						"type:text|name:nome|id:nome|class:form-control required|placeholder:Informe o seu nome|title:Informe o seu nome", 
+					"type:div", //fechando a div que envolve o input
 
-				];
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:sobrenome|value:Sobrenome|obrigatorio:*", //Label do input
+						//input text
+						"type:text|name:sobrenome|id:sobrenome|class:form-control required|placeholder:Informe o seu sobrenome|title:Informe o seu sobrenome", 
+					"type:div", //fechando a div que envolve o input
+
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:email|value:E-mail|obrigatorio:*", //Label do input
+						//input email
+						"type:email|name:email|id:email|maxlength:80|label:E-mail|obrigatorio:*|pattern:[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$|title:Preencha esse campo com o seu e-mail|placeholder:Informe o seu e-mail|class:form-control required|",
+					"type:div", //fechando a div que envolve o input
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:senha|value:Informe uma senha para futuros acessos|obrigatorio:*", //Label do input
+						"type:password|name:senha|id:senha|maxlength:12|label:Senha para futuros acessos|obrigatorio:*|title:Informe uma senha para futuros acessos|placeholder:Informe uma senha para futuros acessos|class:form-control required", //
+					"type:div", //fechando a div que envolve o input
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:celular|value:Celular|obrigatorio:*", //Label do input
+						//input tel
+						"type:tel|name:celular|id:celular|label:Celular|obrigatorio:*|pattern:\([0-9]{2}\)[0-9]{5}-[0-9]{4}$|title:Preencha esse campo com o número do seu celular|placeholder:(00)00000-0000|class:form-control required",
+					"type:div", //fechando a div que envolve o input
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:data_nascimento|value:Data de nascimento|obrigatorio:*", //Label do input
+						//input date
+						"type:date|name:data_nascimento|id:data_nascimento|maxlength:10|min:1940-01-01|max:2020-12-04|obrigatorio:*|value:|pattern:[0-9]{2}\/[0-9]{2}\/[0-9]{4}$|title:Preencha esse campo com a sua data de nascimento|placeholder:|div-class:form-group mt-3|class:form-control required",
+					"type:div",	//fechando a div que envolve o input				
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:sexo|value:Sexo|obrigatorio:*", //Label do input
+						//input select
+						"type:select|name:sexo|id:sexo|options:Selecione-Selecione F-Feminino M-Masculino|class:form-control required",
+					"type:div", //fechando a div que envolve o input
+
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						"type:label|for:mensagem|value:Mensagem|obrigatorio:*", //Label do input
+						//textearea
+						"type:textarea|name:mensagem|id:mensagem|class:form-control required|placeholder:Escreva a sua mensagem|title:Escreva a sua mensagem",
+					"type:div", //fechando a div que envolve o input
+
+
+					//Parágrafo
+					"type:p|class:paragrafo-form|value:Selecione uma opção",
+
+					"type:div|class:form-check",  //abrindo div que irá envolver o input
+						//input radio
+						"type:radio|name:item|id:servico|value:1|title:Serviço|class:form-check-input required-radio",
+						"type:label|for:servico|value:Serviço|class:form-check-label|obrigatorio:*", //Label do input
+					"type:div",	//fechando a div que envolve o input 
+
+					"type:div|class:form-check", //abrindo div que irá envolver o input	
+					    //input radio									
+						"type:radio|name:item|id:desenvolvimento|value:2|title:Desenvolvimento|class:form-check-input required-radio",
+						"type:label|for:desenvolvimento|value:Desenvolvimento|class:form-check-label|obrigatorio:*", //Label do input
+					"type:div",	//fechando a div que envolve o input 	
+
+					"type:div|class:form-check",  //abrindo div que irá envolver o input
+						//input radio
+						"type:radio|name:item|id:suporte|value:3|title:Suporte|class:form-check-input required-radio",
+						"type:label|for:suporte|value:Suporte|class:form-check-label|obrigatorio:*", //Label do input					
+					"type:div",	//fechando a div que envolve o input 
+
+
+					//Parágrafo
+					"type:p|class:paragrafo-form mt-4|value:Informe como prefere ser informado sobre nossas novidades",
+					
+					"type:div|class:form-check",  //abrindo div que irá envolver o input
+					    //input checkbox
+						"type:checkbox|name:contatoEmail|id:contatoEmail|value:1|checked:|label:E-mail|title:Contato por e-email|class:form-check-input required-checkbox",	
+						"type:label|for:contatoEmail|value:Desejo receber novidades por e-mail|class:form-check-label|obrigatorio:*", //Label do input
+					"type:div",	//fechando a div que envolve o input
+
+					"type:div|class:form-check",  //abrindo div que irá envolver o input
+					    //input checkbox
+						"type:checkbox|name:contatoSms|id:contatoSms|value:2|title:Contato por SMS|class:form-check-input required-checkbox",	
+						"type:label|for:contatoSms|value:Desejo receber novidades por SMS|class:form-check-label|obrigatorio:*", //Label do input
+					"type:div",	//fechando a div que envolve o input								
+
+					
+					"type:div|class:form-group mt-3",  //abrindo div que irá envolver o input
+						//input color
+						"type:label|for:cor|value:Clique para selecionar uma cor", //Label do input
+						"type:color|name:cor|id:cor|value:#A52A2A|class:form-control cor",
+					"type:div",	//fechando a div que envolve o input
+
+					//Input submit	
+					"type:submit|name:enviar|id:enviar|value:Enviar|class:btn btn-primary btn-lg btn-block mt-4 mb-5",
+
+						];
 
 				
 				use Core\{CriaArrayTrait,GeraFormularioClass};
@@ -67,7 +135,8 @@
 				$formulario = new GeraFormularioClass();
 				$formulario->render($dados_form); 
 			?>
-			
+
+
 		</div>	
 	</div>	
 
@@ -75,4 +144,3 @@
 
 </body>
 </html>
-
