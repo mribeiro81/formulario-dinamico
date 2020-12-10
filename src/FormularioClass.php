3e6109331b9f4b71cb483dcd1fcc9d599cf65aeb
mribeiro81 <mribeiro81@hotmail.com>
2020-12-10 15:52:19 -0300

    Alterei o método verificaArray

--- src/FormularioClass.php
+++ src/FormularioClass.php
@@ -6,22 +6,21 @@ use Src\FormularioAbstract;
 class FormularioClass extends FormularioAbstract
 {
 	
-	public function render($dados_form)
+	public function render(array $dados_form)
 	{
 
-		$verifica_parametro = $this->verificaArray($dados_form, "render");
+		parent::verificaArray($dados_form, "render");
 		$dados_form = $this->preparaArray($dados_form);		
 		$html = $this->htmlForm($dados_form);
 
-		echo $html;	
-
+		echo $html;
 	}
 
 
 	protected function htmlForm(array $dados_form){
 
-		$verificaParametro = $this->verificaArray($dados_form, "htmlForm");
-		$inputs_metodo_coringa = $this->inputsMetodoCoringa();
+		parent::verificaArray($dados_form, "htmlForm");
+		$inputs_metodo_coringa = parent::inputsMetodoCoringa();
 
 		$html1="";
 		$html2="";
@@ -31,31 +30,31 @@ class FormularioClass extends FormularioAbstract
 			switch ($campo['type']) {
 				
 				case 'form':
-						$html1.= $this->form($campo);
+						$html1.= parent::form($campo);
 					break;					
 
 				case 'label':
-						$html2.= $this->label($campo);
+						$html2.= parent::label($campo);
 					break;										
 
 				case (in_array($campo['type'], $inputs_metodo_coringa)):
-						$html2.= $this->coringa($campo);
+						$html2.= parent::coringa($campo);
 					break;					
 				
 				case 'textarea':
-						$html2.= $this->textarea($campo);
+						$html2.= parent::textarea($campo);
 					break;
 
 				case 'select':
-						$html2.= $this->select($campo);
+						$html2.= parent::select($campo);
 					break;
 
 				case 'div':
-						$html2.= $this->div($campo);
+						$html2.= parent::div($campo);
 					break;
 
 				case ('p' || 'span'):
-						$html2.= $this->paragrafoESpan($campo);
+						$html2.= parent::paragrafoESpan($campo);
 					break;	
 
 				default:
@@ -75,7 +74,7 @@ class FormularioClass extends FormularioAbstract
 	protected function preparaArray(array $dados_form)
 	{
 
-		$verificaParametro = $this->verificaArray($dados_form, "preparaArray");
+		parent::verificaArray($dados_form, "preparaArray");
 
 		$array_retornar= [];
 		foreach ($dados_form as $value) {
@@ -94,24 +93,6 @@ class FormularioClass extends FormularioAbstract
 
 		return $array_retornar;
 	} 
-
-
-	protected function inputsMetodoCoringa()
-	{
-
-		return ['submit', 'button', 'text', 'radio', 'checkbox', 'hidden', 'email', 'tel', 'password', 'date', 'file', 'range', 'color', 'reset' , 'week', 'image', 'month', 'time', 'datetime-local', 'search', 'url'];
-	}
-
-
-	protected function verificaArray($dados, $metodo)
-	{
-
-		if(count($dados) < 1){
-			echo "O método ".$metodo."() da classe GeraFormulario precisa receber como parâmetro um array válido.";
-			exit();
-		}
-
-	}
 
 	
 }
